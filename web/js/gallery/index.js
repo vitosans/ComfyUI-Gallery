5aa7d97ff4c8ecf3f15717e0f65d64f1f37b8059
Vito Sansevero <vito.sansevero@axon-networks.com>
2025-03-05 10:28:39 -0800

    feat(gallery): add index.js for modular gallery init

---
+++ web/js/gallery/index.js
@@ -0,0 +1,10 @@
+/**
+ * ComfyUI Gallery - Index File
+ * This is the main entry point for the modular gallery implementation
+ */
+
+// Import from gallery-core.js, which imports other modules
+import { Gallery } from './gallery-core.js';
+
+// Export to be used in the main gallery_ui.js file
+export { Gallery };
