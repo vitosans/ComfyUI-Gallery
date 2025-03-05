33270499210a78023469948c7ece492f024b6e77
Vito Sansevero <vito.sansevero@axon-networks.com>
2025-03-05 11:14:53 -0800

    refactor(gallery): encapsulate gallery instance logic

--- web/js/gallery/index.js
+++ web/js/gallery/index.js
@@ -7,7 +7,17 @@
 import { Gallery } from './gallery-core.js';
 import { app } from "../../../scripts/app.js";
 
-let gallery;
+// Export the Gallery class and a helper function to set the gallery instance
+export { Gallery };
 
-// Export the Gallery class for use in other modules
-export { Gallery, gallery };
+// Use a module-level variable that isn't directly exported
+let _gallery = null;
+
+// Export functions to get and set the gallery instance
+export function setGalleryInstance(instance) {
+    _gallery = instance;
+}
+
+export function getGalleryInstance() {
+    return _gallery;
+}
